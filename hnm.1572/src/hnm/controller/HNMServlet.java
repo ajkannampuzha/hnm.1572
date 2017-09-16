ccbc886aa60e2a960288b05c8d8bb1a79a0ed883
ajkannampuzha <ajkannampuzha@gmail.com>
2017-09-16 12:18:31 +0530

    adding gson library

--- hnm.1572/src/hnm/controller/HNMServlet.java
+++ hnm.1572/src/hnm/controller/HNMServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
-
+//uncomment for json impl
 /*import com.google.gson.Gson;*/
 
 import java.util.*;
